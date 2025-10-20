1491b8d0f86f67c888acde7eb6c482a8d93b721c
Ignacio Esteves <205209845+ttteclasss@users.noreply.github.com>
2025-10-20 15:47:02 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ export * from "./models/bird";
 export * from "./models/user";
 export * from "./models/comment";
 export * from "./models/birdVisualTrait";
-export*from"./models/birdSighting";
+export * from "./models/birdSighting";
